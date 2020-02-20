e91fb79d438a9e6167576be3d1dc87b7997df924
pavelp21 <pavelvp2182@yandex.ru>
2020-02-20 16:25:45 +0500

    Javascript and base styles are added

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,110 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
+//@prepros-prepend owl.carousel.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
 
-
-
-/*Конец документа*/
+    /*Отзывы*/
+    $(function () {
+        $('#video-revs').owlCarousel({
+            slideSpeed: 200,
+            paginationSpeed: 200,
+            items: 1,
+            loop: true,
+            margin: 20,
+            nav: true,
+            navText: ["‹", "›"],
+            dots: true
+            // autoHeightClass: 'owl-height',
+            // autoHeight: true
+        });
+    });
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+    /* Видео */
+    $(".video-wrapper-rev img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?mute=1&autoplay=1&rel=0" allowfullscreen></iframe>')
+    });
+    $(function () {
+        $('#video-revs .owl-prev, #video-revs .owl-next').click(function () {
+            $('.video-wrapper-rev iframe').each(function () {
+                var l = $(this).parent().attr('data-img');
+                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            });
+            $(".video-wrapper-rev img").click(function () {
+                var a = $(this).parent().attr("data-youtube");
+                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?mute=1&autoplay=1&rel=0" allowfullscreen></iframe>')
+            });
+        });
+    })
+    /*Конец документа*/
 });
